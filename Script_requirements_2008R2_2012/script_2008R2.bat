b2bf5677fa2bd89163f56c1b2fc3a7eff676ee5d
Sheinimi <46713966+Sheinimi@users.noreply.github.com>
2020-02-03 14:03:34 +0100

    Update script_2008R2.bat

--- Script_requirements_2008R2_2012/script_2008R2.bat
+++ Script_requirements_2008R2_2012/script_2008R2.bat
@@ -3,8 +3,7 @@ set currentpath=%~dp0
 
 if "%~1" neq "" goto :%~1
 
-PowerShell Set-ExecutionPolicy ByPass -Force
-PowerShell ./Install_requirements_2008R2.ps1 -Verbose
+PowerShell ./Install_requirements_2008R2_2012.ps1 -Verbose
 echo "Requirements installed."
 Pause
 
@@ -14,10 +13,9 @@ goto :eof
 :part2
 PowerShell %currentpath%/Install_winrm.ps1 -Verbose
 echo "WinRM installed."
-PowerShell Set-ExecutionPolicy Restricted
 
-echo "Deleting Install_requirements_2008R2.ps1"
-del %currentpath%\Install_requirements_2008R2.ps1 /f /q
+echo "Deleting Install_requirements_2008R2_2012.ps1"
+del %currentpath%\Install_requirements_2008R2_2012.ps1 /f /q
 
 echo "Deleting Install_winrm.ps1"
 del %currentpath%\Install_winrm.ps1 /f /q
