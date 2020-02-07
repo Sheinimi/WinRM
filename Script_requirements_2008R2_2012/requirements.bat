215655d08ec666c3f051d7fdd56278a156fb62a2
Sheinimi <46713966+Sheinimi@users.noreply.github.com>
2020-02-07 10:37:27 +0100

    Update requirements.bat

--- Script_requirements_2008R2_2012/requirements.bat
+++ Script_requirements_2008R2_2012/requirements.bat
@@ -1,3 +1,6 @@
-PowerShell ./Install_requirements_2008R2_2012.ps1 -Verbose
+@echo off
+set currentpath=%~dp0
+
+PowerShell %currentpath%/Install_requirements_2008R2_2012.ps1 -Verbose
 echo "Requirements installed."
 Pause
